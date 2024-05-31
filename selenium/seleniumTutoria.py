98fe17cbe34fe207ff817853e97660e4555f3d45
Carlos Matos <carlos.1997matos@gmail.com>
2024-05-31 17:08:49 -0300

    find element

--- selenium/seleniumTutoria.py
+++ selenium/seleniumTutoria.py
@@ -1,3 +1,11 @@
 "source https://www.youtube.com/watch?v=Xjv1sY630Uc&list=PLzMcBGfZo4-n40rB1XaJ0ak1bemvlqumQ"
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
-#teste
+driver = webdriver.Chrome()
+driver.get("https://www.seleniumeasy.com/testng-tutorials")
+driver.implicitly_wait(30)
+my_element = driver.find_element(By.ID,'headingTwo')
+print(f"{my_element.text}")
+my_element.click()
